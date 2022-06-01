4f7ef6a54c087cdfc5ee72cc64d915e6374d97b5
MUHAMMAD_USMAN <105670679+msumanned@users.noreply.github.com>
2022-06-01 15:45:28 +0500

    Add files via upload

---
+++ Lab 1/usman.js
@@ -0,0 +1,7 @@
+
+alert("Great");
+alert("Error! please enter a valid password");
+alert("Welcome to js \n happy coding");
+alert("Welcome to js");
+alert("Happy coding!");
+alert("Precent this page from additional dialog ");
